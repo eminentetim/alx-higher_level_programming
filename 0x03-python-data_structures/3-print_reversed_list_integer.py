e28796da05c20aefb739cf68d05dfc2c93edba83
etimeminent@gmail.com <etimeminent@gmail.com>
2023-10-09 14:15:18 -0700

    deleting two files

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++
@@ -1,6 +0,0 @@
-#!/urs/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list:
-        my_list.reverse()
-        for j in range (len(my_list)):
-            print("{:d}".format(my_list[j]))
