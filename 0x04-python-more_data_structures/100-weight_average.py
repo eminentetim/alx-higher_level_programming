396205be4d80ebdace56e9a0797312c130a15c37
etimeminent@gmail.com <etimeminent@gmail.com>
2023-10-11 21:39:33 -0700

    Writing a function that deletes keys with a specific value in a dictionary.

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list:
+    weight = 0
+    product = []
+    if len(my_list) == 0:
         return 0
-    num = 0
-    den = 0
-
     for i in my_list:
-        num += i[0] * i[1]
-        den += i[1]
-        return(num / den)
+        weight += i[1]
+        product.append(i[0] * i[1])
+    return sum(product) / weight
