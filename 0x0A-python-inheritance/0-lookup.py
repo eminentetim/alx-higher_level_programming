54520d89e711ccf3a75f43463f8ca37c2472c394
etimeminent@gmail.com <etimeminent@gmail.com>
2023-11-05 22:59:36 -0800

    creating  a new directory

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pyhton3
+
+"""
+This script define
+"""
+
+def lookup(obj):
+    """
+    This  function return all the list of available attributes
+    """
+
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class MyList(list):
+    """
+    This class is a subclass of the built_in list class 
+    """
+
+    def print_sorted(self):
+        """
+        This prints a sorted list in an ascending order.
+        """
+
+        print(sorted(self))
