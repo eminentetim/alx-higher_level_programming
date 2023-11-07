f8736bd596548ddf50173473089d28f88f1ec986
etimeminent@gmail.com <etimeminent@gmail.com>
2023-11-06 20:20:54 -0800

    updating task 1

--- 0x0A-python-inheritance/1-main.py
+++ 0x0A-python-inheritance/1-main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 MyList = __import__('1-my_list').MyList
-"""
 my_list = MyList()
 my_list.append(1)
 my_list.append(4)
@@ -10,18 +9,3 @@ my_list.append(5)
 print(my_list)
 my_list.print_sorted()
 print(my_list)
-"""
-
-let_see = MyList()
-let_see.append(6)
-let_see.append(3)
-let_see.append(1)
-let_see.append(5)
-let_see.append(10)
-let_see.append(9)
-let_see.append(2)
-let_see.append(7)
-let_see.append(23)
-print(let_see)
-let_see.print_sorted()
-print(let_see)
