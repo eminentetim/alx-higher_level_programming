815073098c449ed261c49edb4dfb0c18c76633b8
etimeminent@gmail.com <etimeminent@gmail.com>
2023-11-06 20:12:46 -0800

    updating task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,12 @@
+
+
 #!/usr/bin/python3
+"""Defines an subclass or child list class MyList."""
+
 
 class MyList(list):
-    """
-    This class is a subclass of the built_in list class
-    """
+    """This class is a subclass of the built-in list class."""
 
     def print_sorted(self):
-        """
-        This prints a sorted list in an ascending order.
-        """
-
+        """This prints a sorted list in an ascending order."""
         print(sorted(self))
