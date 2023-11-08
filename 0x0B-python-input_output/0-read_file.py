3b31a918391a668d67d55bf397e220c9c8538523
etimeminent@gmail.com <etimeminent@gmail.com>
2023-11-07 17:57:28 -0800

    Write a function that reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,7 @@ Write a function that reads a text file (UTF8)
 and prints it to stdout
 '''
 
+
 def read_file(filename=""):
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
