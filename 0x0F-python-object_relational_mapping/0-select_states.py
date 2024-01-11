9cc37b774714bf54c6ebfe3d35fac7a005ddbf9b
etimeminent@gmail.com <etimeminent@gmail.com>
2024-01-11 09:26:29 -0800

    Writing a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        db_name = sys.argv[3]
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+        cusor = db.cursor()
+        cusor.execute("select * from states order by id")
+        for rows in cusor.fetchall():
+            print(rows)
+        cusor.close()
+        db.close()
