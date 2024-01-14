37e9a696139fb270477b35bd9e84f86f0a2330e7
etimeminent@gmail.com <etimeminent@gmail.com>
2024-01-14 00:27:29 -0800

    Creating a script that is safe from sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> \
+                            <mysql password> \
+                             <database name> \
+                             <state namme searched>
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    if len(sys.argv) == 5:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        db_name = sys.argv[3]
+        db = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=username,
+                passwd=password,
+                db=db_name,
+                charset="utf8")
+        c = db.cursor()
+        c.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(sys.argv[4]))
+        for state in c.fetchall():
+            print(state)
+            ''' Close the cursor and database connection'''
+        c.close()
+        db.close()
