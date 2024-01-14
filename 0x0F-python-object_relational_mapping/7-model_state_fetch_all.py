d245a2e229c87ade8a33cb610231767573f2aefe
etimeminent@gmail.com <etimeminent@gmail.com>
2024-01-14 08:58:39 -0800

    Write a script that prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,7 +11,7 @@ from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                             format(argv[1], argv[2], argv[3], pool_pre_ping=True))
-    Session =  sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     for state in session.query(State).order_by(State.id):
