ba1c348aa3ba0f8999a6424ca0be64ffa10bab7c
etimeminent@gmail.com <etimeminent@gmail.com>
2024-01-14 03:45:45 -0800

    Creating a script that list all the cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,36 +1,21 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa.
-Usage: ./0-select_states.py <mysql username> \
-                            <mysql password> \
-                             <database name> \
-                             <state namme searched>
-"""
-import sys
+'''
+Lists all cities from the database hbtn_0e_4_usa.
+'''
+
 import MySQLdb
+from sys import argv
 
-if __name__ == '__main__':
-    if len(sys.argv) == 5:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        db_name = sys.argv[3]
-        db = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user=username,
-                passwd=password,
-                db=db_name,
-                charset="utf8")
-        c = db.cursor()
-        c.execute("SELECT cities.id, cities.name, states.name \
-                     FROM cities \
-                     JOIN states \
-                        ON cities.states = states.id \
-                     ORDER BY cities.id")
-    
-        for state in c.fetchall():
-            print(state)
-            ''' Close the cursor and database connection'''
-        c.close()
-        db.close()
+if __name__ == "__main__":
 
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN "
+                "states ON cities.state_id = states.id")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> \
+                            <mysql password> \
+                             <database name> \
+                             <state namme searched>
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    if len(sys.argv) == 5:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        db_name = sys.argv[3]
+        state = sys.argv[4]
+        db = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=username,
+                passwd=password,
+                db=db_name,
+                charset="utf8")
+        c = db.cursor()
+        c.execute("SELECT cities.name \
+                     FROM cities \
+                     JOIN states \
+                     ON cities.states_id = states.id \
+                     WHERE states.name  {':s'} \
+                     ORDER BY cities.id")
+    
+        for state in c.fetchall():
+            print(state)
+            ''' Close the cursor and database connection'''
+        c.close()
+        db.close()
+
